8c58b8d4dbe1879532ba9b70af612ba48b1cb6e6
Mabo Giqwa <mabogiqwa1@gmail.com>
2024-12-30 02:17:28 +0200

    Commitment issues resolved

---
+++ car.js
@@ -0,0 +1,16 @@
+class Car{
+    constructor(x,y,width,height){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+    }
+
+    draw(ctx){
+        ctx.beginPath();
+        //draws path by connecting a set of points 
+        ctx.rect(this.x-this.width/2, this.y-this.height/2,this.width,this.height);
+        ctx.fill();
+        //Fills the current path
+    }
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+const canvas=document.getElementById("myCanvas");
+//The above line of code retrieves the HTML element with attribute 'myCanvas'
+canvas.height=window.innerHeight;
+//Above line of code sets the height of canvas to match height of visible browser length
+canvas.width=210
+//Above line of code sets width of canvas to 2
+
+const ctx =canvas.getContext("2d");
+/*Above line of code allows us to render 2D shapes on the canvas.
+ctx variable represents this drawing context*/
+const car=new Car(100,100,30,50);
+/*The first two parameters represent the initial position of the car 
+and the last two represent the dimensions of the car*/
+car.draw(ctx);
+//The draw method renders the 2D shape
