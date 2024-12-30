a9fef0b480ff03982186ed34f38214e27b3f6b7a
Mabo Giqwa <mabogiqwa1@gmail.com>
2024-12-30 22:21:35 +0200

    Added some physics

--- car.js
+++ car.js
@@ -5,6 +5,11 @@ class Car{
         this.width=width;
         this.height=height;
 
+        this.speed=0;
+        this.acceleration=0.2;
+        this.maxSpeed=3;
+        this.friction=0.05;
+
         this.controls=new Controls();
     }
 
@@ -12,11 +17,25 @@ class Car{
     based on user keyboard interaction*/
     update() {
         if (this.controls.forward){
-            this.y = this.y - 2;
+            //this.y = this.y - 2;
+            this.speed+=this.acceleration;
         }
         if (this.controls.reverse){
-            this.y = this.y + 2;
+            this.speed-=this.acceleration;
         }
+        if (this.speed > this.maxSpeed){
+            this.speed=this.maxSpeed;
+        }
+        if (this.speed < (-this.maxSpeed/2)){
+            this.speed=-this.maxSpeed/2;
+        }
+        if (this.speed > 0){
+            this.speed-=this.friction;
+        }
+        if (this.speed < 0){
+            this.speed+=this.friction;
+        }
+        this.y-=this.speed;
     }
 
     draw(ctx){
