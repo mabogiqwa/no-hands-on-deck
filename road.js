4f8edfaa8b76b44c718609a3e3e5e22635f2c474
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-01-01 12:25:45 +0200

    Added moving highway motion and border code

--- road.js
+++ road.js
@@ -1,3 +1,4 @@
+//Feature addition: Add a curved road
 class Road{
     constructor(x,width,laneCount=4){
         this.x=x;
@@ -10,6 +11,11 @@ class Road{
         const infinity=1000000;
         this.top=-infinity;
         this.bottom=infinity;
+    }
+
+    getLaneCenter(laneIndex){
+        const laneWidth=this.width/this.laneCount;
+        return this.left+laneWidth/2+Math.min(laneIndex,this.laneCount-1)*laneWidth;
     }
 
     draw(ctx){
